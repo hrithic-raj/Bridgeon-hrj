2bee9175994ec8eb832f8b23037cbedf4de56fe4
Hrithic <hrithicraj730@gmail.com>
2024-08-02 14:54:07 +0530

    portfolio3

--- portfolio2/gpt/hcp.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.progress-circle').forEach(function(circle) {
-    const percentage = circle.getAttribute('data-percentage');
-    circle.style.setProperty('--percentage', percentage);
-    circle.querySelector('.progress-number').textContent = percentage + '%';
-});
